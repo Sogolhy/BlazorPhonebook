8f44d8aec1675939e292ce6a616dc25bdcc455b5
Saeed Azari <s.azari@tajan.tech>
2023-04-18 14:10:31 +0330

    sync

---
+++ BlazorPhoneBook/Shared/SuccessNotification.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorPhoneBook.Shared
+{
+    public partial class SuccessNotification
+    {
+        public string _modalDisplay;
+        public string _modalClass;
+        public bool _showBackdrop;
+
+        [Inject]
+        public NavigationManager Navigation { get; set; }
+
+        public void Show()
+        {
+            _modalDisplay = "block;";
+            _modalClass = "show";
+            _showBackdrop = true;
+            StateHasChanged();
+        }
+
+        private void Hide()
+        {
+            _modalDisplay = "none;";
+            _modalClass = "";
+            _showBackdrop = false;
+            StateHasChanged();
+        }
+    }
+}
